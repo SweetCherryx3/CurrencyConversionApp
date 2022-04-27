8c9d2493f37baa3439c36ecf2f275af73cc41d44
Sweet Cherryx3 <83660826+SweetCherryx3@users.noreply.github.com>
2022-04-27 10:23:47 -0400

    Update MainActivity.java

--- app/src/main/java/com/example/lab_4/MainActivity.java
+++ app/src/main/java/com/example/lab_4/MainActivity.java
@@ -28,24 +28,21 @@ public class MainActivity extends AppCompatActivity {
         getSupportActionBar().setLogo(R.mipmap.ic_launcher);
         getSupportActionBar().setDisplayUseLogoEnabled(true);
         final EditText USD_Amt = (EditText) findViewById(R.id.USD_Amount);
-        final RadioButton Euros = (RadioButton) findViewById(R.id.conversion1); //was washes
-        final RadioButton MxnPesos = (RadioButton) findViewById(R.id.conversion2); //was exterior and vaccume
+        final RadioButton Euros = (RadioButton) findViewById(R.id.conversion1);
+        final RadioButton MxnPesos = (RadioButton) findViewById(R.id.conversion2);
         final RadioButton CAD_dollars = (RadioButton) findViewById(R.id.conversion3);
         final TextView result = (TextView) findViewById(R.id.totalConversionAmount);
         Button calculate = (Button) findViewById(R.id.btnCost);
 
         calculate.setOnClickListener(new View.OnClickListener(){
-            //@SuppressLint("SetTextI18n")
             @Override
             public void onClick(View view) {
                 USD_Amount = Double.parseDouble(USD_Amt.getText().toString());
                 DecimalFormat currency = new DecimalFormat("$###,###.##");
                 if (Euros.isChecked()){
                     if (USD_Amount > 99_999){
-                        //System.out.println("Amount too large to calculate.");
                         Toast.makeText(MainActivity.this, "Amount too large to calculate.", Toast.LENGTH_LONG).show();
                     } else {
-                        //Toast.makeText(MainActivity.this,"Must input 12 or more washes for a discount.", Toast.LENGTH_LONG).show();
                         totalConversion = USD_Amount * oneEuro;
                         result.setText(String.format("%s for %s Euros.", currency.format(USD_Amount), totalConversion));
                     }
@@ -53,10 +50,8 @@ public class MainActivity extends AppCompatActivity {
                 }
                 if (MxnPesos.isChecked()) {
                     if (USD_Amount > 99_999) {
-                        //System.out.println("Amount too large to calculate.");
                         Toast.makeText(MainActivity.this, "Amount too large to calculate.", Toast.LENGTH_LONG).show();
                     } else {
-                        //Toast.makeText(MainActivity.this, "Must input 12 or more washes for a discount.", Toast.LENGTH_LONG).show();
                         totalConversion = USD_Amount * oneMexicanPeso;
                         result.setText(String.format("%s for %s Mexican Pesos.", currency.format(USD_Amount), totalConversion));
                     }
@@ -64,12 +59,8 @@ public class MainActivity extends AppCompatActivity {
 
                 if (CAD_dollars.isChecked()) {
                     if (USD_Amount > 99_999) {
-                        //System.out.println("Amount too large to calculate.");
-                        //totalCost2 = numberOfWashes * exteriorWithVaccumMore12;
-                        //result.setText(String.format("%s for %s washes.", currency.format(totalCost2), numberOfWashes));
                         Toast.makeText(MainActivity.this, "Amount too large to calculate.", Toast.LENGTH_LONG).show();
                     } else {
-                        //Toast.makeText(MainActivity.this, "Must input 12 or more washes for a discount.", Toast.LENGTH_LONG).show();
                         totalConversion = USD_Amount * oneCAD;
                         result.setText(String.format("%s for %s Canadian Dollars.", currency.format(USD_Amount), totalConversion));
                     }
